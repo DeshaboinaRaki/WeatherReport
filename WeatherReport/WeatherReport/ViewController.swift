21091487068f856dbb7fa09937706259c0dce7ce
DeshaboinaRaki <rakeshios123@gmail.com>
2023-04-16 11:41:09 +0530

    inishall commit

--- WeatherReport/WeatherReport/ViewController.swift
+++ WeatherReport/WeatherReport/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("test this git")
         // Do any additional setup after loading the view.
     }
 
